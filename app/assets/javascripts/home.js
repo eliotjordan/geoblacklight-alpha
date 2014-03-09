45c5f13c834de32662b15350178304b03d6fdffa
Jack Reed <pjreed@stanford.edu>
2014-03-09 10:28:15 -0700

    replaced all tabs with 2 spaces to follow rails convention

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,16 +3,16 @@
 var map;
 
 function setupMap() {
-	map = L.map('map');
-	var basemap = L.tileLayer('https://a.tiles.mapbox.com/v3/examples.map-vyofok3q/{z}/{x}/{y}.png', {
+  map = L.map('map');
+  var basemap = L.tileLayer('https://a.tiles.mapbox.com/v3/examples.map-vyofok3q/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
     maxZoom: 18
-	}).addTo(map);
-	map.setZoom(1)
-	map.setView([0,0])
+  }).addTo(map);
+  map.setZoom(1)
+  map.setView([0,0])
 }
 
 //Setup map on doc ready
 $(document).ready(function(){
-	setupMap();
+  setupMap();
 });
