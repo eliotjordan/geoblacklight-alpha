3db8b95156969d506aee370558d6aea38217da38
Eliot Jordan <eliotj@princeton.edu>
2014-07-03 11:58:35 -0400

    update basemap provider
    Mapbox tile set no longer exists.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -4,8 +4,8 @@ var map;
 
 function setupMap() {
   map = L.map('map');
-  var basemap = L.tileLayer('https://a.tiles.mapbox.com/v3/examples.map-vyofok3q/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+  var basemap = L.tileLayer('http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, Tiles Courtesy of <a href="http://www.mapquest.com/" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png">',
     maxZoom: 18
   }).addTo(map);
   map.setZoom(1)
